fb9ead3bbffccc28c1860297661100df725c7a7a
Brayan <ocamposource@gmail.com>
2026-05-09 08:49:35 -0500

    feat: add nvim-cmp plugin config

---
+++ after/plugin/nvim-cmp.lua
@@ -0,0 +1,44 @@
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+	formatting = {
+		formatting = {
+			format = lspkind.cmp_format({
+				mode = "symbol", -- show only symbol annotations
+				maxwidth = {
+					menu = 50, -- leading text (labelDetails)
+					abbr = 50, -- actual suggestion item
+				},
+				ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+				before = function(entry, vim_item)
+					return vim_item
+				end,
+			}),
+		},
+	},
+})
