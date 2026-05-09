8a791f89a8afebf4a66167ed6d770ec8f33c4f1e
Brayan <ocamposource@gmail.com>
2026-05-08 21:27:11 -0500

    feat: add colorscheme, tabs and auto commands config

--- init.lua
+++ init.lua
@@ -6,3 +6,19 @@ opt = vim.opt
 g = vim.g
 
 require("ocampo")
+
+-- colorscheme
+cmd([[colorscheme oxide]])
+vim.opt.clipboard = "unnamedplus"
+
+-- tabs
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.expandtab = true
+vim.o.softtabstop = 2
+
+-- Auto commands
+api.nvim_create_autocmd("BufEnter", {
+	pattern = "term://*",
+	command = "startinsert",
+})
